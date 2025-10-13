8a93542d2adc59155c8f2fc396161906cc712f06
Faycal Amrani <famrani@alldigitalnetwork.com>
2025-10-13 15:57:50 +0200

    with Ines account

--- server/src/trading-bot.ts
+++ server/src/trading-bot.ts
@@ -126,6 +126,7 @@ switch (accountFlag) {
   case "A": ibAccount = "U7914923"; ibPort = 7496; break;
   case "S": ibAccount = "DUH673915"; ibPort = 7497; break;
   case "F": ibAccount = "U11743275"; ibPort = 7496; break;
+  case "I": ibAccount = "U6466931"; ibPort = 7496; break;
 }
 const ib = new IBApi({ host: "127.0.0.1", port: ibPort, clientId });
 
